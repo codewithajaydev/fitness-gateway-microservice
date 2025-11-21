93d199fbd7c8f2bde14e319d493ca300aa15cd8d
codewithajaydev <ajaybecse03@gmail.com>
2025-11-21 14:33:12 +0530

    push

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# ---------- STAGE 1: Build ----------
+FROM maven:3.9.6-eclipse-temurin-17 AS builder
+WORKDIR /build
+
+COPY pom.xml .
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+# ---------- STAGE 2: Runtime ----------
+FROM eclipse-temurin:17-jdk-alpine
+WORKDIR /app
+
+# copy jar
+COPY --from=builder /build/target/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/fitness/gateway/config/JwtConfig.java
@@ -0,0 +1,46 @@
+package com.fitness.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() throws SSLException {
+
+        SslContextBuilder sslContextBuilder = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE);
+
+        HttpClient httpClient = HttpClient.create()
+                .secure(ssl -> {
+                    try {
+                        ssl.sslContext(sslContextBuilder.build());
+                    } catch (SSLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
+        String jwkSetUri =
+                "https://13.201.82.148:8443/realms/fitness-oauth2/protocol/openid-connect/certs";
+
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri)
+                .webClient(webClient)
+                .build();
+    }
+}
