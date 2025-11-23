cf0388c127f5c1922f7a7c0d622f5e8e5a2c148c
codewithajaydev <ajaybecse03@gmail.com>
2025-11-23 21:53:08 +0530

    pushhh

--- src/main/java/com/fitness/gateway/user/WebClientConfig.java
+++ src/main/java/com/fitness/gateway/user/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.fitness.gateway.user;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,15 +8,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
-    @Bean
-    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder
-                .baseUrl("http://USER-SERVICE")
+    public WebClient userServiceWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://user-fitness-microservices.onrender.com")
                 .build();
     }
 }
